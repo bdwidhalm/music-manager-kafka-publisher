a89f6cd0c26f29c8a7535ecb1326156d02ddbe5d
Brian Widhalm <bdwidhalm@gmail.com>
2025-10-01 16:45:30 -0600

    project initilization

---
+++ src/main/java/com/widhalm/music_manager_kafka_publisher/MusicManagerKafkaPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.widhalm.music_manager_kafka_publisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicManagerKafkaPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicManagerKafkaPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/widhalm/music_manager_kafka_publisher/MusicManagerKafkaPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widhalm.music_manager_kafka_publisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicManagerKafkaPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
